53d8759d71e3ba7454c1d1198587ce0b11e69d09
LDongGyu <ehdrbchd@naver.com>
2019-12-30 20:00:35 +0900

    init proj

---
+++ app/src/main/java/com/example/ocock/Splash.kt
@@ -0,0 +1,20 @@
+package com.example.ocock
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.content.ContextCompat.startActivity
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
