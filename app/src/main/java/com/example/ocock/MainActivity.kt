60594057e48d197078da67ebf7abe3468732dc28
LDongGyu <ehdrbchd@naver.com>
2020-01-04 15:29:06 +0900

    메인화면 '오늘의 칵테일은?' 화면 추가

--- app/src/main/java/com/example/ocock/MainActivity.kt
+++ app/src/main/java/com/example/ocock/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.ocock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+
+        search_img.setOnClickListener {
+            Toast.makeText(applicationContext,"search",Toast.LENGTH_SHORT).show()
+        }
+
+        star_img.setOnClickListener {
+            Toast.makeText(applicationContext,"star",Toast.LENGTH_SHORT).show()
+        }
+
+        etc_img.setOnClickListener {
+            Toast.makeText(applicationContext,"etc",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/ocock/Splash.kt
+++ app/src/main/java/com/example/ocock/Splash.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
 import androidx.core.content.ContextCompat.startActivity
 
 class Splash : AppCompatActivity() {
